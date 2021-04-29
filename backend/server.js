2d91ddebcfafe16136748c6f6f82511fefeaa37b
khalilsayhi <khalil.sayhi1@esprit.tn>
2021-04-29 17:43:57 +0200

    user signup and auth w JWT along with product management

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+dotenv.config()
+
+connectDB()
+//initialize express
+const app = express()
+
+app.use(express.json())
+
+//create a route
+app.get('/', (req, res) => {
+  res.send('API IS RUNNING...')
+})
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+)
